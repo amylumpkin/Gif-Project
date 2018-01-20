b465d4899067c5daf3be2a19316be9dd4b306acb
Amy Lumpkin <amylumpkin@amys-mbp.attlocal.net>
2018-01-20 18:44:51 -0500

    everything works except still/animate on click

--- starting-over/gif-take-2.js
+++ starting-over/gif-take-2.js
@@ -1,5 +1,40 @@
+$(document).ready(function() {
 
-	$("button").on("click", function(){
+//create starting array
+var topics = ["tom waits", "marilyn monroe", "harrison ford", "barack obama", "donald trump"];
+
+//function to create initial buttons
+function renderButtons(){
+	$("#buttons-go-here").empty(); //so there won't be repeat buttons
+	//loop through array
+	for (var i = 0; i < topics.length; i++){
+		//dynamically create buttons for all items in array
+		var a = $("<button>");
+		//add class
+		a.addClass("topics");
+		//add attribute & value of array item at index i
+		a.attr("data-search", topics[i]);
+		//button's text
+		a.text(topics[i]);
+		//inserting buttons into HTML
+		$("#buttons-go-here").append(a);
+	}
+
+}
+//adding new buttons
+$("#add-button").submit(function(event){ 
+event.preventDefault();
+    //grab html from what is typed into box
+	var textBox = $("#input").val().trim();
+	topics.push(textBox);
+	renderButtons();
+	console.log(topics);
+});
+
+renderButtons();
+
+//function gets the GIFs when a button is clicked
+	$(document).on("click", ".topics", function(){
 		var x = $(this).data("search");
 		console.log(x);
 
@@ -13,7 +48,28 @@
 					$("#gifs-go-here").prepend("<p>Rating: " + response.data[i].rating+"<p>");
 					$("#gifs-go-here").prepend("<img src='" + response.data[i].images.downsized.url + "'>");
 				}
-				
 			})
-	})
+				
+			});
+/*
+	//add still/animated thing here.....
+$(".gif").on("click", function() {
+  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+  var state = $(this).attr("data-state");
+  
+  // If image is still, animate on click, else (if it's animated) make still on click.
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
+*/
+
+
+	});
+
+
 
